8a40134835eeb96777b53a4343024508399fc805
Adam Norton <adam@adamnorton.com>
2019-11-15 13:03:51 -0600

    added dependencies for heroku.

---
+++ client/build/precache-manifest.6bc84686f13c5cfb9ab95cd4145ddece.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ac0cf9b4a857acecad0",
+    "url": "/static/js/main.cbfa1056.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c2b21aaf4fd081e22bb",
+    "url": "/static/js/2.4e9c7dce.chunk.js"
+  },
+  {
+    "revision": "85553c9b57454c8592fc5da651f4974e",
+    "url": "/index.html"
+  }
+];
